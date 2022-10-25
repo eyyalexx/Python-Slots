86279a1b7fa4e8011c88341246d7e5668e26e331
Alex Grigorev <alex.grigorev@hotmail.com>
2022-10-25 10:40:20 -0400

    Update main.py
    Get number of lines to bet on.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+MAX_LINES = 3
+
 
 def deposit():
     while True:
@@ -13,5 +15,26 @@ def deposit():
             print("Please enter a number.")
     return amount
 
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid amount of lines.")
+        else:
+            print("Please enter a number.")
+    return lines
 
-deposit()
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+
+
+main()
+
+
